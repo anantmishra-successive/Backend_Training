f25982fca41e74a87d0dab31ab6a6360f6e40f34
anantmishra-successive <anant.mishra@successive.tech>
2024-04-23 09:44:50 +0530

    add:assignment1

--- Day1/index.js
+++ Day1/index.js
@@ -1,6 +1,10 @@
 const {sum,subtract,multiply,divide} = require('./lib/math');
 
 
+
+const fs = require("fs");
+
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,6 +20,19 @@ rl.question("Enter the first number: ", (firstNumber) => {
    console.log(subtract(parsedFirst, parsedSecond));
    console.log(multiply(parsedFirst, parsedSecond));
    console.log(divide(parsedFirst, parsedSecond).toFixed(2));
+   const data = `
+Opeartion , Result,
+add ${sum([parsedFirst, parsedSecond])}
+subtract : ${subtract(parsedFirst, parsedSecond)}
+multiply : ${multiply(parsedFirst, parsedSecond)}
+division : ${divide(parsedFirst, parsedSecond).toFixed(2)}
+`;
+
+fs.writeFile("data.csv", data, "utf-8", (err) => {
+  if (err) console.log(err);
+  else console.log("Data saved");
+});
+
    
     rl.close();
   });
