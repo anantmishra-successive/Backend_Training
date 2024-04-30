ec5edfbca51ecd862a4bcf43f6143e12e48ef2f7
anantmishra-successive <anant.mishra@successive.tech>
2024-04-30 22:56:29 +0530

    add:custommiddleware

--- Day4/app.js
+++ Day4/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const { faker } = require('@faker-js/faker');
 const {logRequestDetails} = require('./middlewares/logrequestdetails')
 const { errorHandler } = require('./middlewares/errorHandler')
+const { customHeaderMiddleware } = require('./middlewares/customHeaderMiddleware')
+const {rateLimitMiddleware} = require('./middlewares/rateLimitMiddleware')
 const app = express();
 const PORT =  3000;
 const postRoutes=require("./routes/postRoutes")
 
 app.use(express.json());
 app.use(logRequestDetails)
+app.use(customHeaderMiddleware)
+// app.use(rateLimitMiddleware)
 app.use('/posts',postRoutes)
+
+
+app.get('/custom',rateLimitMiddleware,(req,res,next)=>{
+  res.send("hello world");
+})
 app.get('*',(req,res,next)=>{
   try {
     throw new Error('Something went wrong! fix it');
